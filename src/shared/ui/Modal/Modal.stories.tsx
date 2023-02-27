39248fc5fb536abf5ac71fa74a124ef5c830c168
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2023-02-28 00:39:29 +0300

    fix test ui

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -19,10 +19,3 @@ Primary.args = {
   isOpen: true,
   children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
-
-export const Dark = Template.bind({});
-Dark.args = {
-  isOpen: true,
-  children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
-};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
